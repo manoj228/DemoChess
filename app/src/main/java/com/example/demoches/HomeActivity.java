2799ca2ab274bd8e0fa8d2c8e0a9d21363ee1361
manoj kumar <manoj22.be@gmail.com>
2020-12-15 08:25:00 +0530

    save changes

--- app/src/main/java/com/example/demoches/HomeActivity.java
+++ app/src/main/java/com/example/demoches/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.demoches;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -12,13 +15,82 @@ public class HomeActivity extends AppCompatActivity {
     TextView wrook1, wkngiht1, wbishop1, wqueen, wking, wrook2, wknight2, wbishop2;
     TextView wpawn1, wpawn2, wpawn3, wpawn4, wpawn5, wpawn6, wpawn7, wpawn8;
 
+
+    Button btEnd;
+    TextView p1, p2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        initialize();
+        btEnd = findViewById(R.id.btnEndGame);
+        p1 = findViewById(R.id.game_over);
+        p2 = findViewById(R.id.game_over2);
 
+        btEnd.setVisibility(View.INVISIBLE);
+        p1.setVisibility(View.INVISIBLE);
+        p2.setVisibility(View.INVISIBLE);
+
+        btEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            }
+        });
+
+        initialize();
+        setValues();
+        checkForValidMove();
+
+    }
+
+
+
+    private void setValues() {
+        // black coins
+        // for the first row
+        brook1.setBackgroundResource(R.drawable.rookblack);
+        bkngiht1.setBackgroundResource(R.drawable.knightblack);
+        bbishop1.setBackgroundResource(R.drawable.bishopblack);
+        bqueen.setBackgroundResource(R.drawable.queenblack);
+        bking.setBackgroundResource(R.drawable.kingblack);
+        bbishop2.setBackgroundResource(R.drawable.bishopblack);
+        bknight2.setBackgroundResource(R.drawable.knightblack);
+        brook2.setBackgroundResource(R.drawable.rookblack);
+
+        // for second row
+        bpawn1.setBackgroundResource(R.drawable.pawnblack);
+        bpawn2.setBackgroundResource(R.drawable.pawnblack);
+        bpawn3.setBackgroundResource(R.drawable.pawnblack);
+        bpawn4.setBackgroundResource(R.drawable.pawnblack);
+        bpawn5.setBackgroundResource(R.drawable.pawnblack);
+        bpawn6.setBackgroundResource(R.drawable.pawnblack);
+        bpawn7.setBackgroundResource(R.drawable.pawnblack);
+        bpawn8.setBackgroundResource(R.drawable.pawnblack);
+
+
+        // white coins
+        // for the last row
+        wrook1.setBackgroundResource(R.drawable.rookwhite);
+        wkngiht1.setBackgroundResource(R.drawable.knightwhite);
+        wbishop1.setBackgroundResource(R.drawable.bishopwhite);
+        wqueen.setBackgroundResource(R.drawable.queenwhite);
+        wking.setBackgroundResource(R.drawable.kingwhite);
+        wbishop2.setBackgroundResource(R.drawable.bishopwhite);
+        wknight2.setBackgroundResource(R.drawable.knightwhite);
+        wrook2.setBackgroundResource(R.drawable.rookwhite);
+
+
+        // for the seventh row
+        wpawn1.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn2.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn3.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn4.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn5.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn6.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn7.setBackgroundResource(R.drawable.pawnwhite);
+        wpawn8.setBackgroundResource(R.drawable.pawnwhite);
     }
 
     void initialize()
